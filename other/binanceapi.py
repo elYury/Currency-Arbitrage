61a8f0a3186ecd2f5600e788ebac809539a13f79
elYury <132591650+elYury@users.noreply.github.com>
2023-09-23 12:49:39 +0200

    Delete other directory

--- other/binanceapi.py
+++
@@ -1,9 +0,0 @@
-from binance.client import Client
-
-api_key = "gKHnjT4bo0aZlqwVMp1XK25kXoVCQkoo8Iu9bNrqrwgwAbpP2m2WPJ0ruSvtJb1q"
-api_secret = "0R5Y9IMzovnsb9cAmdH1EqsNFb5xLKqORPtV4lvAqFJW6iY53bd6soQ0KJtrf3S3"
-
-client = Client(api_key, api_secret)
-print("loggged in")
-
-
