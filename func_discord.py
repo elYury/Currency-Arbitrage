deb595eace247d842b3affa2e8bc13878fea383a
Yury Pravdin <132591650+elYury@users.noreply.github.com>
2023-09-29 18:11:43 +0200

    Update func_discord.py by removing personal data

--- func_discord.py
+++ func_discord.py
@@ -1,6 +1,4 @@
 
-# https://discord.com/api/v9/channels/1117711348747477075/messages
-
 import requests
 
 def send_discord(text):
@@ -9,7 +7,7 @@ def send_discord(text):
     }
 
     header = {
-        'authorization': 'MTExNzczMzg0MTg5Mzg3MTYxNw.Gh9JSN.RTOe-NTcHW9nZlcVfKc6DEqBEFQ6zUuntbu5YM'
+        'authorization': ' TYPE YOUR AUTHORIZATION CODE IN HERE'
     }
 
     r = requests.post("https://discord.com/api/v9/channels/1117711348747477075/messages", 
